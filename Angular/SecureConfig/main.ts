02653a6304e5f987fb3121dd1e6f2d7cf05c7cf4
Ahmed Khaled <164809257+Akmahmoud2024@users.noreply.github.com>
2024-06-22 06:26:39 +0300

    Create main.ts

---
+++ Angular/SecureConfig/main.ts
@@ -0,0 +1,15 @@
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+import { enableProdMode } from "@angular/core";
+import { ConfigService } from "thiqah-resources";
+
+const envConfigService = new ConfigService<any>();
+
+envConfigService.init().then(() => {
+  if (envConfigService.getOne("production")) {
+    enableProdMode();
+  }
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
+});
