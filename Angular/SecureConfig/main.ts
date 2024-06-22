21c5fa47fb6c08a8153d699d2d2ce51fbb9469a9
Ahmed Khaled <164809257+Akmahmoud2024@users.noreply.github.com>
2024-06-22 09:53:31 +0300

    Update main.ts

--- Angular/SecureConfig/main.ts
+++ Angular/SecureConfig/main.ts
@@ -2,8 +2,9 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
 import { enableProdMode } from "@angular/core";
 import { ConfigService } from "thiqah-res";
+import { AppConfig } from "./shared/models/app-config";
 
-const envConfigService = new ConfigService<any>();
+const envConfigService = new ConfigService<AppConfig>();
 
 envConfigService.init().then(() => {
   if (envConfigService.getOne("production")) {
